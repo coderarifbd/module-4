f2c6fe84e47ccb4a55e1c21e1dacf7641b899bdb
coderarifbd <coder.arifbd@gmail.com>
2023-02-14 11:00:59 +0600

    array function

--- Pre-record Video/array.php
+++ Pre-record Video/array.php
@@ -6,7 +6,19 @@
         'Roni Farzana'
     );
 
+    
+    //Mutable 
+    $students[2] = "shafik";
     $n = count($students);
     for($i=0; $i<$n; $i++){
         echo $students[$i].PHP_EOL;
     }
+
+    /**
+     * array function 
+     * array_shift() - remove array first element and return the element
+     * array_unshift() - add a new item in array to first element
+     * array_pop() - return last array element and remove from array
+     * array_push() - Add new item in array to last elements
+     * 
+    */
